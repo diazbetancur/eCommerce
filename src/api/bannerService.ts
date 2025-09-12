bcdf6e11961287b2e22f056e492653185528d4cf
Esteban Gutierrez <diazbetancur@gmail.com>
2025-09-11 19:06:04 -0500

    update services

--- src/api/bannerService.ts
+++ src/api/bannerService.ts
@@ -2,6 +2,5 @@ import api from './apiService';
 
 export async function fetchBanners() {
   const response = await api.get('/Banner');
-  console.log(response.data.length);
   return response.data;
 }
--- src/api/categoryService.ts
+++ src/api/categoryService.ts
@@ -2,6 +2,6 @@ import api from './apiService';
 
 export async function fetchCategories() {
   const response = await api.get('/Category');
-  console.log(response.data.length);
+
   return response.data;
 }
